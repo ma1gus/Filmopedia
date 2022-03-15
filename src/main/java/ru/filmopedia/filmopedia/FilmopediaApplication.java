1160a1cd1422fe2b6ba0cd5f39f36ea8368e73da
AkiraItoSan <akiraitosan99@gmail.com>
2022-03-15 22:28:44 +0700

    init

---
+++ src/main/java/ru/filmopedia/filmopedia/FilmopediaApplication.java
@@ -0,0 +1,13 @@
+package ru.filmopedia.filmopedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmopediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmopediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/filmopedia/filmopedia/FilmopediaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.filmopedia.filmopedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmopediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
