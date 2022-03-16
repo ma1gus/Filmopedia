917541d9233ab8d2974ab1fe841091aa47ebb556
Nikita <Headstrong97@mail.ru>
2022-03-16 22:55:06 +0300

    КОНТРОЛЛЕР ОБНОВЛЕНИЕ

--- src/main/java/ru/filmopedia/filmopedia/config/IMDbSearchConfig.java
+++ src/main/java/ru/filmopedia/filmopedia/config/IMDbSearchConfig.java
@@ -7,30 +7,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class IMDbSearchConfig {
 
     private final static String URL_SEARCH_FILM = "https://imdb-api.com/en/API/SearchMovie/k_zima5p9v/";
-/*
-1)урл Константа \/
-2)метод с мапой
-3)метод с листом фильмами
- */
 
-    private static Map<String, Object> getInputStream(String filmName) {
+    private static Map<String, Object> getInputStream(URL url) {
         Map<String, Object> result = null;
         try {
-            HttpURLConnection httpURLConnection = null;
-            URL url = new URL(URL_SEARCH_FILM + filmName);
-            httpURLConnection = (HttpURLConnection) url.openConnection();
-
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
-
             result = new ObjectMapper().readValue(bufferedReader, HashMap.class);
             bufferedReader.close();
         } catch (IOException e) {
@@ -39,6 +30,7 @@ public class IMDbSearchConfig {
         return result;
     }
 
+    @SuppressWarnings("unchecked")
     public static List<Film> getFilmsBySearch(String filmName) {
         List<Map<String,String>> nachaloResult = (List<Map<String, String>>) getInputStream(filmName).get("results");
 
--- src/main/java/ru/filmopedia/filmopedia/controller/IMDbController.java
+++ src/main/java/ru/filmopedia/filmopedia/controller/IMDbController.java
@@ -6,6 +6,7 @@ import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.filmopedia.filmopedia.config.IMDbSearchConfig;
 import ru.filmopedia.filmopedia.models.Film;
@@ -16,17 +17,23 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Controller
 public class IMDbController {
+
+    private List<Film> list = IMDbSearchConfig.getFilmsBySearch("nachalo");
+
     @GetMapping("/")
-    public String nachalo(@RequestParam(value = "film_name") String filmName, Model model) {
-        model.addAttribute("films", IMDbSearchConfig.getFilmsBySearch(filmName));
+    public String nachalo(Model model) {
+        model.addAttribute("films", list);
         return "index";
+    }
+
+    @GetMapping("/search")
+    public String getFilmFromSearch(@RequestParam(value = "film_name") String filmName, Model model) {
+        list = IMDbSearchConfig.getFilmsBySearch(filmName);
+        return "redirect:/";
     }
 
 }
